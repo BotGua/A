71951e6f3c389acdf1efae85efb421b72850568c
BotGua <74773543+BotGua@users.noreply.github.com>
2020-11-21 13:56:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,41 +31,6 @@ const
    ReconnectMode,
    ProxyAgent,
    waChatKey,
-
-const qrcode = require("qrcode-terminal");
-const moment = require("moment");
-const cheerio = require("cheerio");
-const get = require('got')
-const fs = require("fs");
-const dl = require("./lib/downloadImage.js");
-const fetch = require('node-fetch');
-const urlencode = require("urlencode");
-const axios = require("axios");
-const imageToBase64 = require('image-to-base64');
-const menu = require("./lib/menu.js");
-const donate = require("./lib/donate.js");
-const info = require("./lib/info.js");
-//
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
-const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
-//
-const
-{
-   WAConnection,
-   MessageType,
-   Presence,
-   MessageOptions,
-   Mimetype,
-   WALocationMessage,
-   WA_MESSAGE_STUB_TYPES,
-   ReconnectMode,
-   ProxyAgent,
-   waChatKey,
-	
 } = require("@adiwajshing/baileys");
 var jam = moment().format("HH:mm");
 
